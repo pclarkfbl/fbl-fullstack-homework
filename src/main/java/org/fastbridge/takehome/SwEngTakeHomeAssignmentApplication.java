fc4d9ee5fcb0ae7b412ec23b0aceb16c0930d796
Vivek Ranjan <vivek@fastbridge.org>
2017-11-15 13:16:24 -0600

    Initial commit

---
+++ src/main/java/org/fastbridge/takehome/SwEngTakeHomeAssignmentApplication.java
@@ -0,0 +1,12 @@
+package org.fastbridge.takehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwEngTakeHomeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwEngTakeHomeAssignmentApplication.class, args);
+	}
+}
