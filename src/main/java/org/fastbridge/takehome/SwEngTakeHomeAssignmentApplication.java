cf28353401152991ea49746687c61481395862b7
Vivek Ranjan <vivek@fastbridge.org>
2017-11-15 15:36:09 -0600

    Added: JSPs support

--- src/main/java/org/fastbridge/takehome/SwEngTakeHomeAssignmentApplication.java
+++ src/main/java/org/fastbridge/takehome/SwEngTakeHomeAssignmentApplication.java
@@ -2,9 +2,10 @@ package org.fastbridge.takehome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SwEngTakeHomeAssignmentApplication {
+public class SwEngTakeHomeAssignmentApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwEngTakeHomeAssignmentApplication.class, args);
